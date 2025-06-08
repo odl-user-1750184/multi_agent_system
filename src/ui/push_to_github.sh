5f83e82a6c4f2e2880296bc2ac8ad08f5fc935cc
odl-user-1750184 <odl_user_1750184@sandboxailabs1011.onmicrosoft.com>
2025-06-08 17:32:57 +0000

    normal push

--- src/ui/push_to_github.sh
+++ src/ui/push_to_github.sh
@@ -7,7 +7,7 @@ set -e
 git add index.html
 
 # Commit with a default message
-git commit -m "Auto-update: index.html pushed from multi-agent system"
+git commit -m "Auto-update: new index.html pushed from multi-agent system"
 
 # Push to the remote repository
 git push https://$GITHUB_USERNAME:$GITHUB_PAT@github.com/odl-user-1750184/azure_multi-agent.git main
